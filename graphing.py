6031598d268b3ffbec55d568fd5e47cd0168a44c
Lee Reid <contact@musink.net>
2021-05-13 20:00:36 +0200

    2d and 3d scatter working but no trendline

--- graphing.py
+++ graphing.py
@@ -11,18 +11,50 @@ import plotly.graph_objects as graph_objects
 
 # Set the default theme 
 template =  graph_objects.layout.Template() 
-template.layout = graph_objects.Layout(margin=dict(l=2, r=2, b=2, t=2),
+template.layout = graph_objects.Layout(
+                                    # border width
+                                    margin=dict(l=2, r=2, b=2, t=2),
+                                    # Interaction
                                     hovermode="closest",
+                                    # axes
                                     xaxis_showline=True,
                                     xaxis_linewidth=2,
                                     yaxis_showline=True,
-                                    yaxis_linewidth=2
+                                    yaxis_linewidth=2,
+                                    # Pick a slightly different P.O.V from default
+                                    # this avoids the extremities of the y and x axes
+                                    # being cropped off
+                                    scene_camera=dict(eye=dict(x=1.5, y=1.5, z=0.1))
                                     )    
                                     
 template.data.scatter = [graph_objects.Scatter(marker=dict(opacity=0.8))]
+template.data.scatter3d = [graph_objects.Scatter3d(marker=dict(opacity=0.8))]
+
 
 pio.templates["custom_template"] = template
-pio.templates.default = "plotly_white+custom_template" #  
+pio.templates.default = "plotly_white+custom_template"
+
+def _to_human_readable(text:str):
+    return text.replace("_", " ")
+
+def _prepare_labels(df:pandas.DataFrame, labels:List[Optional[str]]):
+    '''
+    Ensures labels are human readable. 
+    Automatically picks data if labels not provided explicitly
+    '''
+
+    human_readable = {}
+
+    for i in range(len(labels)):
+        lab = labels[i]
+        if lab is None:
+            lab = df.columns[i]
+        labels[i] = lab
+
+        # make human-readable
+        human_readable[lab] = _to_human_readable(lab)
+    
+    return labels, human_readable
 
 def scatter_2D(df:pandas.DataFrame, 
                 label_x:Optional[str]=None, 
@@ -39,25 +71,16 @@ def scatter_2D(df:pandas.DataFrame,
     '''
 
     # Automatically pick columns if not specified
-    if label_x is None:
-        label_x = df.columns[0]
-    if label_y is None:
-        label_y = df.columns[1]
+    selected_columns, axis_labels = _prepare_labels(df, [label_x, label_y])
 
-    # Create human-readable labels
-    x_axis_label = label_x.replace('_', ' ')
-    y_axis_label = label_y.replace('_', ' ')
-
+    print(axis_labels)
 
     # Create the figure and plot
     fig = px.scatter(df, 
-                x=label_x, 
-                y=label_y, 
+                x=selected_columns[0], 
+                y=selected_columns[1], 
                 color=label_colour, 
-                labels={
-                    label_x:x_axis_label,
-                    label_y:y_axis_label,
-                    },
+                labels=axis_labels,
                 title=title
                 )
 
@@ -65,14 +88,6 @@ def scatter_2D(df:pandas.DataFrame,
     size = int(round(22.0 - 20/(1+exp(-(df.shape[0]/100-2)))))
     print(size)
     fig.update_traces(marker={'size': size})
-
-    # # Remove most whitespace from the edges
-    # # Set the background colour
-    # fig.update_layout(margin=dict(l=2, r=2, b=2, t=2))
-
-    # # Colour the vertical and horizontal lines 
-    # fig.update_xaxes(showline=True, linewidth=AXIS_LINE_WIDTH)
-    # fig.update_yaxes(showline=True, linewidth=AXIS_LINE_WIDTH)
 
 
     # Show the plot, if requested
@@ -99,37 +114,23 @@ def scatter_3D(df:pandas.DataFrame,
     '''
 
     # Automatically pick columns if not specified
-    if label_x is None:
-        label_x = df.columns[0]
-    if label_y is None:
-        label_y = df.columns[1]
-    if label_z is None:
-        label_z = df.columns[2]
-    if label_colour is None:
-        label_colour = label_z
+    selected_columns, axis_labels = _prepare_labels(df, [label_x, label_y, label_z])
 
-    # Create human-readable labels
-    x_axis_label = label_x.replace('_', ' ')
-    y_axis_label = label_y.replace('_', ' ')
-    z_axis_label = label_z.replace('_', ' ')
+    if label_colour is None:
+        # Colour by the Z dimension
+        label_colour = selected_columns[2]
+    else:
+        axis_labels[label_colour] = _to_human_readable(label_colour)
 
     # Create the figure and plot
     fig = px.scatter_3d(df, 
-                x=label_x, 
-                y=label_y, 
-                z=label_z,
+                x=selected_columns[0], 
+                y=selected_columns[1], 
+                z=selected_columns[2],
                 color=label_colour,
-                labels={
-                    label_x:x_axis_label,
-                    label_y:y_axis_label,
-                    label_z:z_axis_label
-                    },
-                title=title                )
+                labels=axis_labels,
+                title=title)
 
-    # Pick a slightly different P.O.V from default
-    # this avoids the extremities of the y and x axes
-    # being cropped off
-    fig.update_layout(scene_camera=dict(eye=dict(x=1.5, y=1.5, z=0.1)))
 
     # Show the plot, if requested
     if show:
