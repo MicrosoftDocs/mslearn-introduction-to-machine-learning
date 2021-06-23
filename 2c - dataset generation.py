31f9f0f8ad802088ae893da44666ab9bf92ab7cd
Lee Reid <contact@musink.net>
2021-06-23 16:31:14 +0200

    dataset adjustments

--- 2c - dataset generation.py
+++ 2c - dataset generation.py
@@ -16,8 +16,9 @@ tree_color = []
 tree_size = []
 tree_motion = []
 
-n_trees = 4000
-n_dogs = 200
+n_trees = 800
+n_rocks = 800
+n_animals = 200
 n_hikers = 400
 
 np.random.seed(1234567)
@@ -27,34 +28,42 @@ def skewed_normal(skew, n_samples):
     return skewnorm.rvs(skew, size=n_samples)
 
 # Generate trees
-tree_color = rng.choice(["green", "brown", "white", "yellow", "black"], n_trees, p=[0.3, 0.35, 0.25, 0.05, 0.05])
+tree_color = rng.choice(["green", "brown", "white", "grey", "black"], n_trees, p=[0.2, 0.35, 0.25, 0.15, 0.05])
 tree_roughness = (rng.standard_normal(n_trees) * 10 + 30)/30
-tree_motion = rng.standard_normal(n_trees) * 0.5 + 2
+tree_motion = skewed_normal(0.4, n_trees) * 0.5
 tree_size = rng.standard_normal(n_trees) * 6 + (tree_roughness + 1) / (tree_motion + 1) * 20 # size is proportional to roughness as large trees are rough. Small trees also blow more in the wind
 tree_labels = ["tree"] * n_trees
 
 
+# Generate rocks
+rock_color = rng.choice(["green", "brown", "white", "grey", "black"], n_rocks, p=[0.1, 0.25, 0.35, 0.25, 0.05])
+rock_roughness = (rng.standard_normal(n_rocks) * 10 + 30)/30
+rock_motion = rng.uniform(0, 0.05, size=n_rocks) # measurement error
+rock_size = rng.standard_normal(n_rocks) + 1.75
+rock_labels = ["rock"] * n_rocks
+
+
 # Generate hikers
-hike_color = rng.choice(["green", "brown", "white", "red", "blue", "yellow", "orange", "black"], n_hikers, p=[0.2, 0.25, 0.01, 0.04, 0.1, 0.05, 0.05, 0.3])
+hike_color = rng.choice(["green", "brown", "white", "red", "blue", "grey", "orange", "black"], n_hikers, p=[0.2, 0.25, 0.01, 0.04, 0.1, 0.05, 0.05, 0.3])
 hike_roughness = (rng.standard_normal(n_hikers) * 2 + 5)/5
-hike_motion = skewed_normal(0.4, n_hikers) * 1.5 + 4 
+hike_motion = skewed_normal(0.4, n_hikers) * 1.5 + 2
 hike_size = rng.standard_normal(n_hikers) * 0.3 + 1.75
 hike_labels = ["hiker"] * n_hikers
 
 
-# Generate dogs
-dog_color = rng.choice(["brown", "white", "red", "black"], n_dogs, p=[0.3, 0.25, 0.05, 0.4])
-dog_roughness = (rng.standard_normal(n_dogs) * 2 + 4.7)/5 # similar to people
-dog_motion = skewed_normal(0.4, n_dogs) * 3 + 4 # more variable than people
-dog_size = rng.standard_normal(n_dogs) * 0.3 + 1.2
-dog_labels = ["dog"] * n_dogs
+# Generate animals
+animal_color = rng.choice(["brown", "white", "red", "black"], n_animals, p=[0.3, 0.25, 0.05, 0.4])
+animal_roughness = (rng.standard_normal(n_animals) * 2 + 4.7)/5 # similar to people
+animal_motion = skewed_normal(0.4, n_animals) * 3 + 2 # more variable than people
+animal_size = rng.standard_normal(n_animals) * 0.4 + 1.6
+animal_labels = ["animal"] * n_animals
 
 # Concat
-color = np.hstack([tree_color, hike_color, dog_color])
-size = np.hstack([tree_size, hike_size, dog_size])
-roughness = np.hstack([tree_roughness, hike_roughness, dog_roughness])
-motion = np.hstack([tree_motion, hike_motion, dog_motion])
-label = np.hstack([tree_labels, hike_labels, dog_labels])
+color = np.hstack([tree_color, rock_color, hike_color, animal_color])
+size = np.hstack([tree_size, rock_size, hike_size, animal_size])
+roughness = np.hstack([tree_roughness, rock_roughness, hike_roughness, animal_roughness])
+motion = np.hstack([tree_motion, rock_motion, hike_motion, animal_motion])
+label = np.hstack([tree_labels, rock_labels, hike_labels, animal_labels])
 
 # Prevent negative values
 roughness[roughness< 0] = 0
@@ -66,7 +75,7 @@ d = pandas.DataFrame(dict(size=size, roughness=roughness, color=color, motion=mo
 d.to_csv("Data/snow_objects.csv", sep="\t", index=False)
 
 # View as graphs
-if False:
+if True:
     graphing.multiple_histogram(d, label_x="size",  label_group="label", show=True)
     graphing.multiple_histogram(d, label_x="roughness",  label_group="label", show=True)
     graphing.multiple_histogram(d, label_x="motion",  label_group="label", show=True)
@@ -171,7 +180,3 @@ train_sensitivity, test_sensitivity = assess_performance(random_forest)
 
 print("Train Sensitivity:", train_sensitivity)
 print("Test Sensitivity:", test_sensitivity)
-
-
-
-
